9ccdac8ebc022a2528715009a7cdb9285e3ae308
Jingxuan Man <jm@host113.fritz.box>
2016-04-22 14:51:51 +0200

    Initial commit

---
+++ src/main/java/org/jevis/sqldatasource/MySQLDataSource.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2015 NeroBurner; Copyright (C) 2016 JingxuanMan
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation in version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This driver is part of the OpenJEVis project, further project information are
+ * published at <http://www.OpenJEVis.org/>.
+ */
+package org.jevis.sqldatasource;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author NeroBurner (AIT)
+ * @author JingxuanMan (Envidatec)
+ */
+public class MySQLDataSource extends SQLDriverAbstract {
+
+    @Override
+    public String loadJDBC(String host, int port, String schema, String dbUser, String dbPW, String domain) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + schema + "?";
+        _con = DriverManager.getConnection(url, dbUser, dbPW);
+        return url;
+    }
+
+    interface MySQL extends SQLDriverAbstract.SQLServer {
+
+        public final static String NAME = "MySQL Server";
+    }
+
+    @Override
+    protected String getClassName() {
+        return MySQL.NAME;
+    }
+}
---
+++ src/main/java/org/jevis/sqldatasource/MySQLDataSourceMain.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2016 JingxuanMan
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation in version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This driver is part of the OpenJEVis project, further project information are
+ * published at <http://www.OpenJEVis.org/>.
+ */
+package org.jevis.sqldatasource;
+
+import org.jevis.api.JEVisException;
+import org.jevis.api.JEVisObject;
+import org.jevis.api.sql.JEVisDataSourceSQL;
+
+/**
+ *
+ * @author JingxuanMan (Envidatec)
+ */
+public class MySQLDataSourceMain {
+
+    public static void main(String[] args) throws JEVisException {
+        JEVisDataSourceSQL _client = new JEVisDataSourceSQL(
+                "openjevis.org", "13306", "jevis", "jevis", "jevistest");
+        _client.connect("Sys Admin", "JEV34Env");
+        JEVisObject mysqlServer = _client.getObject(4554l);
+        MySQLDataSource ds = new MySQLDataSource();
+        ds.initialize(mysqlServer);
+        ds.run();
+
+    }
+}
